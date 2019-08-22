fa3dba2378c61118e5f2d3a2deb0400d0e8536b9
Leandro <lcantero18@gmail.com>
2019-08-22 18:22:21 -0300

    Ejercicio2

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
@@ -1,8 +1,22 @@
 package ar.edu.unahur.obj2.ejercicio1;
 
-public class Ejercicio1 {
+import java.util.Scanner;
 
-    public int[] ordenar() {
-        return new int[] {};
+public class Ejercicio1 {
+    public static void main (String [] args){
+        numerosInverso();
+    }
+
+    public static void numerosInverso(){
+        int[] numeros = new int[10];
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresar datos numéricos:");
+        for(int i=0; i<numeros.length; i++){
+            System.out.print((i+1)+" - Digite un número: ");
+            numeros[i] = entrada.nextInt();
+        }
+        for(int i= numeros.length-1; i > 0; i--){
+            System.out.println(numeros[i]);
+        }
     }
 }
