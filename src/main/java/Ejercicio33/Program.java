13200029aa2708caca38844a732c86460d47a291
Leandro <lcantero18@gmail.com>
2019-09-03 21:29:56 -0300

    Ejercicio 3.3

--- src/main/java/Ejercicio33/Program.java
+++ src/main/java/Ejercicio33/Program.java
@@ -4,34 +4,61 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Scanner opcion = new Scanner(System.in);
-        int op = opcion.nextInt();
+        Scanner scan = new Scanner(System.in);
 
-        switch (op){
+        System.out.println("1.Crear nuevo alumno");
+        System.out.println("2.Crear nueva asignatura.");
+        System.out.println("3.Matricular alumno en asignatura.");
+        System.out.println("4.Listado de alumnos matriculados en una asignatura.");
+        System.out.println("5.Listado de asignaturas en las que está matriculado un alumno y total de horas.");
+        System.out.println("6.Salir");
 
+        int opcion = scan.nextInt();
+        switch (opcion){
             case 1:
+                System.out.println("Nombre:");
+                String nombreAlumno = scan.next();
+                System.out.println("Apellido:");
+                String apellido = scan.next();
+                System.out.println("Email:");
+                String email = scan.next();
+                crearAlumno(nombreAlumno, apellido, email);
 
+            case 2:
+                System.out.println("Nombre:");
+                String nombreAsignatura = scan.next();
+                System.out.println("Horas semanales:");
+                int horasSemanales = scan.nextInt();
+                crearAsignatura(nombreAsignatura, horasSemanales);
+
+            case 3:
+                System.out.println("Alumno:");
+                Alumno alumno;
+                System.out.println("Asignatura");
+                Asignatura asignatura;
+                matricularAlumno(alumno, asignatura);
         }
+
 
     }
 
-    public Alumno crearAlumno(String nombre, String apellido, String email){
+    public static Alumno crearAlumno(String nombre, String apellido, String email){
         return new Alumno(nombre, apellido, email);
     }
 
-    public Asignatura crearAsignatura(String nombre, int horasSemanales){
+    public static Asignatura crearAsignatura(String nombre, int horasSemanales){
         return new Asignatura(nombre, horasSemanales);
     }
 
-    public void matricularAlumno(Alumno alumno, Asignatura asignatura){
+    public static void matricularAlumno(Alumno alumno, Asignatura asignatura){
         alumno.matricular(asignatura);
     }
 
-    public void listadoMatriculados(Asignatura asignatura){
+    public static void listadoMatriculados(Asignatura asignatura){
         System.out.println("Alumnos matriculados:" + asignatura.getAlumnos());
     }
 
-    public void asignaturasYHorasALumno(Alumno alumno){
+    public static void asignaturasYHorasALumno(Alumno alumno){
         System.out.println("Asignaturas:" + alumno.getAsignaturas() + "Horas totales:" + alumno.horasTotales());
     }
 
