1b26a25fa0286dd032363777406baed3f7f95f4f
Leandro <lcantero18@gmail.com>
2019-09-05 19:56:06 -0300

    Completo

--- src/main/java/Ejercicio33/Program.java
+++ src/main/java/Ejercicio33/Program.java
@@ -4,17 +4,12 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
-        System.out.println("1.Crear nuevo alumno");
-        System.out.println("2.Crear nueva asignatura.");
-        System.out.println("3.Matricular alumno en asignatura.");
-        System.out.println("4.Listado de alumnos matriculados en una asignatura.");
-        System.out.println("5.Listado de asignaturas en las que está matriculado un alumno y total de horas.");
-        System.out.println("6.Salir");
+        menu();
 
         boolean trueOrFalse = true;
         while(trueOrFalse){
+            Scanner scan = new Scanner(System.in);
             int opcion = scan.nextInt();
             switch (opcion){
                 case 1:
@@ -23,8 +18,9 @@ public class Program {
                     System.out.println("Apellido:");
                     String apellido = scan.next();
                     System.out.println("Email:");
-                    String email = scan.nextLine();
+                    String email = scan.next();
                     crearAlumno(nombreAlumno, apellido, email);
+                    menu();
                     break;
 
                 case 2:
@@ -33,10 +29,11 @@ public class Program {
                     System.out.println("Horas semanales:");
                     int horasSemanales = scan.nextInt();
                     crearAsignatura(nombreAsignatura, horasSemanales);
+                    menu();
                     break;
 
                 case 3:
-                    System.out.println("Alumno:");
+                    System.out.println("--------Alumno--------:");
                     System.out.println("Nombre:");
                     String nombre2 = scan.next();
                     System.out.println("Apellido:");
@@ -44,13 +41,14 @@ public class Program {
                     System.out.println("Email:");
                     String email2 = scan.next();
                     Alumno alumno = new Alumno(nombre2, apellido2, email2);
-                    System.out.println("Asignatura");
+                    System.out.println("--------Asignatura--------");
                     System.out.println("Nombre:");
                     String nombreAsig = scan.next();
                     System.out.println("Horas Semanales:");
                     int horasSem = scan.nextInt();
                     Asignatura asignatura = new Asignatura(nombreAsig, horasSem);
                     matricularAlumno(alumno, asignatura);
+                    menu();
                     break;
 
                 case 4:
@@ -61,11 +59,12 @@ public class Program {
                     int horasSem2 = scan.nextInt();
                     Asignatura asignatura1 = new Asignatura(nombreAsig2, horasSem2);
                     listadoMatriculados(asignatura1);
+                    menu();
                     break;
 
                 case 5:
                     System.out.println("Alumno:");
-                    System.out.println("Nombre");
+                    System.out.println("Nombre:");
                     String nombreA = scan.next();
                     System.out.println("Apellido:");
                     String apellidoA = scan.next();
@@ -73,6 +72,7 @@ public class Program {
                     String emailA = scan.next();
                     Alumno alumno1 = new Alumno(nombreA, apellidoA, emailA);
                     asignaturasYHorasALumno(alumno1);
+                    menu();
                     break;
 
                 case 6:
@@ -82,6 +82,7 @@ public class Program {
 
                 default:
                     System.out.println("El numero ingresado no está entre las opciones");
+                    menu();
                     break;
             }
 
@@ -108,5 +109,14 @@ public class Program {
         System.out.println("Asignaturas:" + alumno.getAsignaturas() + "Horas totales:" + alumno.horasTotales());
     }
 
+    public static void menu(){
+        System.out.println("--------------------");
+        System.out.println("1.Crear nuevo alumno");
+        System.out.println("2.Crear nueva asignatura.");
+        System.out.println("3.Matricular alumno en asignatura.");
+        System.out.println("4.Listado de alumnos matriculados en una asignatura.");
+        System.out.println("5.Listado de asignaturas en las que está matriculado un alumno y total de horas.");
+        System.out.println("6.Salir");
+    }
 
 }
